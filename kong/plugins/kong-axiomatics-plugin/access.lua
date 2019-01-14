f5c7490c28774961ed7638d07299a5be782055da
Ioannis Iordanidis <ioannis.iordanidis@optum.com>
2019-01-14 21:28:18 +0000

    Finally, sending a well-formed HTTP request

--- kong/plugins/kong-axiomatics-plugin/access.lua
+++ kong/plugins/kong-axiomatics-plugin/access.lua
@@ -3,14 +3,38 @@ local _M = {}
 function _M.execute(conf)
 
   local sock = ngx.socket.tcp()
-  sock:settimeout(10000)
-  sock:connect("optum.free.beeceptor.com/middleman", 443)
-  sock:sslhandshake(true, host, false)
-  sock:send("foo bar")
+  sock:settimeout(1000)
 
+  local ok, err = sock:connect("optum.proxy.beeceptor.com", 443)
+  if not ok then
+    ngx.log(ngx.ERR, "failed to connect ", err)
+  end
+
+  local ok, err = sock:sslhandshake()
+  if not ok then
+    ngx.log(ngx.ERR,  "failed to do SSL handshake with ", err)
+  else
+    ngx.log(ngx.ERR, "no ssl error: ", tostring(ok))
+  end
+
+  local ok, err = sock:send("GET /middleman HTTP/1.1\r\nHost: optum.proxy.beeceptor.com\r\nConnection: close\r\n\r\n")
+  if not ok then
+    ngx.log(ngx.ERR, "failed to send ", err)
+  else
+    ngx.log(ngx.ERR, "no send error: ", ok)
+  end
+
+  local line, err = sock:receive()
+  if not line then
+    ngx.log(ngx.ERR,  "failed to read response: ", err)
+  else
+    ngx.log(ngx.ERR, "no error, line: ", line)
+  end
+
+-- Insert a Hello World header --
   if conf.say_hello then
     ngx.log(ngx.ERR, "============ Hello World! ============")
-    ngx.header["Hello-World-4"] = "Hello World!"
+    ngx.header["Hello-World-6"] = "Hello World!"
   else
     ngx.log(ngx.ERR, "============ Bye World! ============")
     ngx.header["Hello-World"] = "Bye World!"
