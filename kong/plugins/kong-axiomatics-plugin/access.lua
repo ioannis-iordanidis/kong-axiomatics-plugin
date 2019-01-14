464e87cba126d0ae329ece46a5b9521aab67bf1e
Ioannis Iordanidis <ioannis.iordanidis@optum.com>
2019-01-14 13:41:14 +0000

    Testing the socket

--- kong/plugins/kong-axiomatics-plugin/access.lua
+++ kong/plugins/kong-axiomatics-plugin/access.lua
@@ -1,6 +1,13 @@
 local _M = {}
 
 function _M.execute(conf)
+
+  local sock = ngx.socket.tcp()
+  sock:settimeout(10000)
+  sock:connect("optum.free.beeceptor.com/middleman", 443)
+  sock:sslhandshake(true, host, false)
+  sock:send("foo bar")
+
   if conf.say_hello then
     ngx.log(ngx.ERR, "============ Hello World! ============")
     ngx.header["Hello-World-2"] = "Hello World!"
