4aacbbdfda93cc65ef8c32ad28d75531b271cfac
Ioannis Iordanidis <ioannis.iordanidis@optum.com>
2019-01-16 14:31:16 +0000

    Removed the hello world header injection, and the configuration parameter, and maded chaged to logging and comments only

--- kong/plugins/kong-axiomatics-plugin/access.lua
+++ kong/plugins/kong-axiomatics-plugin/access.lua
@@ -60,43 +60,34 @@ function _M.execute(conf)
   local decoded_token, err = decode_token(token)
   local payload = compose_post_payload(decoded_token, conf)
 
--- Send an empty POST request to a mock --
+-- POST the payload to a listening mock --
   local sock = ngx.socket.tcp()
   sock:settimeout(1000)
 
   local ok, err = sock:connect("optum.proxy.beeceptor.com", 443)
   if not ok then
-    ngx.log(ngx.ERR, "failed to connect ", err)
+    ngx.log(ngx.ERR, "Failed to connect ", err)
   end
 
   local ok, err = sock:sslhandshake()
   if not ok then
-    ngx.log(ngx.ERR,  "failed to do SSL handshake with ", err)
+    ngx.log(ngx.ERR,  "Failed to do SSL handshake with ", err)
   else
-    ngx.log(ngx.ERR, "no ssl error: ", tostring(ok))
+    ngx.log(ngx.ERR, "No ssl error: ", tostring(ok))
   end
 
   local ok, err = sock:send("GET /middleman HTTP/1.1\r\nHost: optum.proxy.beeceptor.com\r\nConnection: close\r\n\r\n")
   if not ok then
-    ngx.log(ngx.ERR, "failed to send ", err)
+    ngx.log(ngx.ERR, "Failed to send ", err)
   else
-    ngx.log(ngx.ERR, "no send error: ", ok)
+    ngx.log(ngx.ERR, "No send error: ", ok)
   end
 
   local line, err = sock:receive()
   if not line then
-    ngx.log(ngx.ERR,  "failed to read response: ", err)
+    ngx.log(ngx.ERR,  "Failed to read response: ", err)
   else
-    ngx.log(ngx.ERR, "no error, line: ", line)
-  end
-
--- Insert a Hello World header --
-  if conf.say_hello then
-    ngx.log(ngx.ERR, "============ Hello World! ============")
-    ngx.header["Hello-World-6"] = "Hello World!"
-  else
-    ngx.log(ngx.ERR, "============ Bye World! ============")
-    ngx.header["Hello-World"] = "Bye World!"
+    ngx.log(ngx.ERR, "No error, line: ", line)
   end
 end
 
--- kong/plugins/kong-axiomatics-plugin/schema.lua
+++ kong/plugins/kong-axiomatics-plugin/schema.lua
@@ -1,6 +1,5 @@
 return {
   fields = {
-    say_hello = {type = "boolean", default = true},
     url = {required = true, type = "string"},
     token_header_name = {type = "string", required = true, default = "Authorization"},
     claims_to_include = {type = "array", default = {".*"}}
