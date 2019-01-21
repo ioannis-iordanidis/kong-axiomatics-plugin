1d66b1aaf7841ac93d353cb1515f7d93b8fa0673
Ioannis Iordanidis <ioannis.iordanidis@optum.com>
2019-01-21 16:20:40 +0000

    Post body payload is now XACML JSON

--- kong/plugins/kong-axiomatics-plugin/access.lua
+++ kong/plugins/kong-axiomatics-plugin/access.lua
@@ -39,17 +39,41 @@ local function decode_token(token)
   return decoded_token, err
 end
 
--- Parse the selected claims out of the JWT to a POST body --
+-- Compose XACML POST body --
 local function compose_post_payload(decoded_token, conf)
+
+  -- AccessSubject XACML values
+  local access_subject_attribute_list = {}
+  local access_subject = {}
   local claims = decoded_token.claims
-  local payload = {}
   for claim_key,claim_value in pairs(claims) do
-    for _,claim_pattern in pairs(conf.claims_to_include) do
+    for i,claim_pattern in pairs(conf.claims_to_include) do
       if string.match(claim_key, "^"..claim_pattern.."$") then
-        payload[claim_key] = claim_value
+        local key_value = {}
+        key_value["AttributeId"] = claim_key
+        key_value["Value"] = claim_value
+        access_subject_attribute_list[i] = key_value
       end
     end
   end
+  access_subject["Attribute"] = access_subject_attribute_list
+
+  -- Action XACML values
+  local action_attribute = {}
+  local action = {}
+  action_attribute["AttributeId"] = "action-id"
+  action_attribute["Value"] = ngx.var.scheme .. "//" .. ngx.var.host .. ngx.var.request_uri
+  action_attribute["DataType"] = "anyURI"
+  action["Attribute"] = action_attribute
+
+  -- Resource XACML values
+
+  -- Payload
+  request = {}
+  payload = {}
+  request["AccessSubject"] = access_subject
+  request["Action"] = action
+  payload["Request"] = request
   ngx.log(ngx.ERR, "Selected claim and values parsed out of the JWT:\n" .. JSON:encode_pretty(payload) .. "\n")
   return JSON:encode_pretty(payload)
 end
